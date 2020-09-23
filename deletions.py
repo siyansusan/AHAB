d59e3005b952e6e1369fb10bda99e450617e16cd
Bill Majoros <martiandna@gmail.com>
2020-09-23 11:42:11 -0400

    update

--- deletions.py
+++ deletions.py
@@ -27,8 +27,8 @@ from SamPairedReadStream import SamPairedReadStream
 from SamHspFactory import SamHspFactory
 from SamHspClusterer import SamHspClusterer
 from SamAnnotation import SamAnnotation
-
-MIN_IDENTITY=0.9
+import pyBigWig
+from ConfigFile import ConfigFile
 
 class Target:
     def __init__(self,ID,pos,seq):
@@ -53,20 +53,42 @@ def getReadEnds(readGroup,end1or2):
         else: reads.append(pair.read2)
     return reads
 
+def getAlignabilities(anno,bigwig,OUT):
+    for hsp in anno.getHSPs():
+        refCoords=hsp.getRefInterval()
+        stats=bigwig.stats(hsp.getRefName(),refCoords.getBegin(),
+                           refCoords.getEnd(),type="min")
+        for x in stats: print(x,file=OUT,flush=True)
+
 #=========================================================================
 # main()
 #=========================================================================
-if(len(sys.argv)!=4):
-    exit(ProgramName.get()+" <filename.sam> <target-sites.txt> <deduplicate:0/1>\n")
-(samFile,targetFile,dedup)=sys.argv[1:]
-dedup=True if dedup==1 else False
+if(len(sys.argv)!=3):
+    exit(ProgramName.get()+" <filename.sam> <settings.config>\n")
+(samFile,configFilename)=sys.argv[1:]
+
+# Process configuration file
+config=ConfigFile(configFilename)
+dedup=configFile.lookupOrDie("DEDUPLICATE")
+dedup=True if dedup=="True" or dedup=="true" or dedup=="T" or dedup=="TRUE" \
+    or dedup=="Yes" or dedup=="yes" else False
+MIN_IDENTITY=float(configFile.lookupOrDie("MIN_IDENTITY"))
+MAX_REF_GAP=float(configFile.lookupOrDie("MAX_REF_GAP"))
+alignabilityMap=configFile.lookupOrDie("ALIGNABILITY")
 
 # Load target locations
-targets=loadTargets(targetFile)
+targets=loadTargets(configFile.lookupOrDie("TARGET_SITES"))
+
+# Open the ENCODE alignability map
+bigwig=pyBigWig.open(alignabilityMap)
 
 # Process SAM file
 hspFactory=SamHspFactory()
 stream=SamPairedReadStream(samFile)
+#numErrors=0
+numGreater1000000=0; numGreater100000=0; numGreater10000=0; numGreater1000=0
+readsKept=0
+ALIGNABILITIES=open("alignabilities.txt","wt")
 while(True):
     readGroup=stream.nextGroup()
     if(readGroup is None): break
@@ -82,22 +104,38 @@ while(True):
     if(not anno.allRefsSame()): continue ### debugging
     if(anno.firstRef()!="chrX"): continue ### debugging
     if(numHSPs<2): continue
+    if(anno.anyRefsOverlap()): continue
     if(not anno.allSameStrand()): continue
     if(anno.lowestPercentIdentity()<MIN_IDENTITY): continue
+    getAlignabilities(anno,bigwig,ALIGNABILITIES)
+
+    readGapLengths=anno.getReadGapLengths()
+    refGapLengths=anno.getRefGapLengths()
+    #if(len(refGapLengths)==0 or max(refGapLengths)<MAX_REF_GAP): continue ###
 
     print(readGroup.getID(),"\t==> after clustering there are ",len(HSPs),
           " HSPs",sep="")
     for hsp in HSPs:
-        print(hsp.toString(),"\t",sep="",end="")
-    print()
-
-    readGapLengths=anno.getReadGapLengths()
-    refGapLengths=anno.getRefGapLengths()
+        print("\t",hsp.toString(),sep="")
     if(len(readGapLengths)>0):
         print("\tREAD GAPS:",",".join([str(x) for x in readGapLengths]))
     if(len(refGapLengths)>0):
         print("\tREF GAPS:",",".join([str(x) for x in refGapLengths]))
-        if(max(refGapLengths)>1000000): 
-            print("\tMAX GAP LENGTH EXCEEDED")
+        m=max(refGapLengths)
+        if(m>1000000): numGreater1000000+=1
+        elif(m>100000): numGreater100000+=1
+        elif(m>10000): numGreater10000+=1
+        elif(m>1000): numGreater1000+=1
+        #if(max(refGapLengths)>MAX_REF_GAP): 
+        #    print("\tMAX GAP LENGTH EXCEEDED")
+            #numErrors+=1
+    readsKept+=1
+    print("----------------------------------------------------------------")
+ALIGNABILITES.close()
 
-    
+#print(numErrors,"total errors")
+print(numGreater1000000,">Mb")
+print(numGreater100000,">100k")
+print(numGreater10000,">10k")
+print(numGreater1000,">1k")
+print("out of",readsKept,"reads kept")
