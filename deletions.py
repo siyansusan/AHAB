a54fe0e561d99649614d3e142501f203395efec3
Bill Majoros <martiandna@gmail.com>
2020-09-28 14:12:21 -0400

    update

--- deletions.py
+++ deletions.py
@@ -13,103 +13,113 @@ from builtins import (bytes, dict, int, list, object, range, str, ascii,
 import sys
 import os
 import ProgramName
-from SamReader import SamReader
-from FastaReader import FastaReader
-from FastaWriter import FastaWriter
-from Translation import Translation
 from Interval import Interval
-from Pipe import Pipe
 from Rex import Rex
 rex=Rex()
-import TempFilename
-from CigarString import CigarString
 from SamPairedReadStream import SamPairedReadStream
 from SamHspFactory import SamHspFactory
 from SamHspClusterer import SamHspClusterer
 from SamAnnotation import SamAnnotation
 import pyBigWig
 from ConfigFile import ConfigFile
+from Ahab import Ahab
 
-class Target:
-    def __init__(self,ID,pos,seq):
-        self.ID=ID
-        self.pos=pos
-        self.seq=seq
+class Analysis(Ahab):
+    def __init__(self,configFile):
+        super().__init__(configFile)
+        config=self.config
+        dedup=config.lookupOrDie("DEDUPLICATE")
+        dedup=True if dedup in ("True","true","T","TRUE","Yes","yes") \
+            else False
+        self.dedup=dedup
+        self.MIN_IDENTITY=float(config.lookupOrDie("MIN_IDENTITY"))
+        self.MAX_REF_GAP=float(config.lookupOrDie("MAX_REF_GAP"))
+        self.MIN_ALIGNABILITY=float(config.lookupOrDie("MIN_ALIGNABILITY"))
+        self.FIRST_CUT_SITE=int(config.lookupOrDie("FIRST_CUT_SITE"))
+        self.SECOND_CUT_SITE=int(config.lookupOrDie("SECOND_CUT_SITE"))
+        self.DELETION_REGION=Interval(self.FIRST_CUT_SITE,self.SECOND_CUT_SITE)
+        self.TARGET_CHROM=config.lookupOrDie("TARGET_CHROM")
+        self.MAX_ANCHOR_DISTANCE=int(config.lookupOrDie("MAX_ANCHOR_DISTANCE"))
+        #self.MAX_ANCHOR_OVERLAP=int(config.lookupOrDie("MAX_ANCHOR_OVERLAP"))
+        self.OUTPUT_DIR=config.lookupOrDie("OUTPUT_DIR")
+        self.prepareOutputFiles(OUTPUT_DIR)
 
-def loadTargets(filename):
-    targets=[]
-    with open(filename,"rt") as IN:
-        for line in IN:
-            fields=line.rstrip().split()
-            if(len(fields)!=3): continue
-            (ID,pos,seq)=fields
-            targets.append(Target(ID,int(pos),seq))
-    return targets
+    def __del__(self):
+        self.ON_TARGET_DELETION.close()
+        self.ON_TARGET_IMPERFECT_DELETION.close()
+        self.ON_TARGET_NO_EDIT.close()
+        self.OFF_TARGET_EDIT.close()
+        self.OFF_TARGET_NO_EDIT.close()
 
-def getReadEnds(readGroup,end1or2):
-    reads=[]
-    for pair in readGroup.getReads():
-        if(end1or2==1): reads.append(pair.read1)
-        else: reads.append(pair.read2)
-    return reads
+    def prepareOutputFiles(self,DIR):
+        if(not os.path.exists(DIR)):
+            os.system("mkdir -p "+DIR)
+        if(not rex.find("(.*)/$",DIR)): DIR=DIR+"/"
+        self.ON_TARGET_DELETION=open(DIR+"bin-on-target-deletion.txt","wt")
+        self.ON_TARGET_IMPERFECT_DELETION=\
+               open(DIR+"bin-on-target-imperfect-deletion.txt","wt")
+        self.ON_TARGET_NO_EDIT=open(DIR+"bin-on-target-no-edit.txt","wt")
+        self.OFF_TARGET_EDIT=open(DIR+"bin-off-target-edit.txt","wt")
+        self.OFF_TARGET_NO_EDIT=open(DIR+"bin-off-target-no-edit.txt","wt")
 
-def getAlignabilities(anno,bigwig):
-    for hsp in anno.getHSPs():
-        refCoords=hsp.getRefInterval()
-        stats=bigwig.stats(hsp.getRefName(),refCoords.getBegin(),
-                           refCoords.getEnd(),type="min")
-        #for x in stats: print(x,file=OUT,flush=True)
-        minValue=min(stats)
-        hsp.setAlignability(minValue)
+    def processCases(self,anno):
+        numHSPs=anno.numHSPs()
+        if(numHSPs==1): self.process1HSP(anno)
+        elif(numHSPs==2): self.process2HSPs(anno)
+        elif(numHSPs==3): self.process3HSPs(anno)
+        else: self.processManyHSPs(anno)
 
-def process1HSP(anno):
-    # Check whether this shows evidence of NO deletion, or whether it
-    # is off-target
-    pass
+    def process1HSP(self,anno):
+        # Check whether this shows evidence of NO deletion, or whether it
+        # is off-target
+        pass
 
-def process2HSPs(anno):
-    if(anno.allSameStrand()): 
-        process2HSPsSameStrand(anno)
-    else:
-        process2HSPsDifferentStrand(anno)
+    def process2HSPs(self,anno):
+        if(anno.allSameStrand()): 
+            self.process2HSPsSameStrand(anno)
+        else:
+            self.process2HSPsDifferentStrand(anno)
 
-def process2HSPsDifferentStrand(anno):
-    # Check for inversions
-    HSPs=anno.getHSPs(); hsp1=HSPs[0]; hsp2=HSPs[1]
-    interval1=hsp1.getRefInterval(); interval2=hsp2.getRefInterval()
-    if(abs(FIRST_CUT_SITE-interval1.getEnd())<=MAX_ANCHOR_DISTANCE and
-       abs(SECOND_CUT_SITE-interval2.getEnd())<=MAX_ANCHOR_DISTANCE or
-       abs(interval1.getBegin()-FIRST_CUT_SITE)<=MAX_ANCHOR_DISTANCE and
-       abs(interval2.getBegin()-SECOND_CUT_SITE)<=MAX_ANCHOR_DISTANCE):
-        print("on-target inversion:",anno.getReadID())
+    def process2HSPsDifferentStrand(self,anno):
+        # Check for inversions
+        HSPs=anno.getHSPs(); hsp1=HSPs[0]; hsp2=HSPs[1]
+        interval1=hsp1.getRefInterval(); interval2=hsp2.getRefInterval()
+        FIRST_CUT_SITE=self.FIRST_CUT_SITE
+        SECOND_CUT_SITE=self.SECOND_CUT_SITE
+        MAX_ANCHOR_DISTANCE=self.MAX_ANCHOR_DISTANCE
+        if(abs(FIRST_CUT_SITE-interval1.getEnd())<=MAX_ANCHOR_DISTANCE and
+           abs(SECOND_CUT_SITE-interval2.getEnd())<=MAX_ANCHOR_DISTANCE or
+           abs(interval1.getBegin()-FIRST_CUT_SITE)<=MAX_ANCHOR_DISTANCE and
+           abs(interval2.getBegin()-SECOND_CUT_SITE)<=MAX_ANCHOR_DISTANCE):
+            self.bin(anno.getReadID(),self.ON_TARGET_INVERSION) ###
+            
+    def process2HSPsSameStrand(self,anno):
+        HSPs=anno.getHSPs(); hsp1=HSPs[0]; hsp2=HSPs[1]
+        interval1=hsp1.getRefInterval(); interval2=hsp2.getRefInterval()
+        gaps=anno.getRefGaps()
+        if(len(gaps)!=1): return
+        gap=gaps[0]
+        if(not gap.overlaps(self.DELETION_REGION)): 
+            bin(anno.getReadID(),self.OFF_TARGET_EDIT)
+            return
+        FIRST_CUT_SITE=self.FIRST_CUT_SITE
+        SECOND_CUT_SITE=self.SECOND_CUT_SITE
+        MAX_ANCHOR_DISTANCE=self.MAX_ANCHOR_DISTANCE
+        d1=FIRST_CUT_SITE-interval1.getEnd()
+        d2=interval2.getBegin()-SECOND_CUT_SITE
+        if(max(abs(d1),abs(d2))>MAX_ANCHOR_DISTANCE): 
+            bin(anno.getReadID(),self.ON_TARGET_IMPERFECT_DELETION)
+            return
+        bin(anno.getReadID(),self.ON_TARGET_DELETION)
 
-    pass
+    def process3HSPs(self,anno):
+        # Check for evidence of integration or inversion at target site, or of
+        # off-target edits
+        pass
 
-def process2HSPsSameStrand(anno):
-    HSPs=anno.getHSPs(); hsp1=HSPs[0]; hsp2=HSPs[1]
-    interval1=hsp1.getRefInterval(); interval2=hsp2.getRefInterval()
-    gaps=anno.getRefGaps()
-    if(len(gaps)!=1): return
-    gap=gaps[0]
-    if(not gap.overlaps(DELETION_REGION)): 
-        print("possible off-target edit:",anno.getReadID())
-        return
-
-    d1=FIRST_CUT_SITE-interval1.getEnd()
-    d2=interval2.getBegin()-SECOND_CUT_SITE
-    if(max(abs(d1),abs(d2))>MAX_ANCHOR_DISTANCE): 
-        print("on-target but imperfect edit:",anno.getReadID())
-        return
-    print("on-target near-perfect deletion:",anno.getReadID())
-
-def process3HSPs(anno):
-    # Check for evidence of integration or inversion at target site, or of
-    # off-target edits
-    pass
-
-def processManyHSPs(anno):
-    # Complex edits: check whether on-target or off-target
-    pass
+    def processManyHSPs(self,anno):
+        # Complex edits: check whether on-target or off-target
+        pass
 
 #=========================================================================
 # main()
@@ -118,38 +128,17 @@ if(len(sys.argv)!=3):
     exit(ProgramName.get()+" <filename.sam> <settings.config>\n")
 (samFile,configFilename)=sys.argv[1:]
 
-# Process configuration file
-config=ConfigFile(configFilename)
-dedup=config.lookupOrDie("DEDUPLICATE")
-dedup=True if dedup=="True" or dedup=="true" or dedup=="T" or dedup=="TRUE" \
-    or dedup=="Yes" or dedup=="yes" else False
-MIN_IDENTITY=float(config.lookupOrDie("MIN_IDENTITY"))
-MAX_REF_GAP=float(config.lookupOrDie("MAX_REF_GAP"))
-alignabilityMap=config.lookupOrDie("ALIGNABILITY")
-MIN_ALIGNABILITY=float(config.lookupOrDie("MIN_ALIGNABILITY"))
-
-# Load target locations
-#targets=loadTargets(config.lookupOrDie("TARGET_SITES"))
-FIRST_CUT_SITE=int(config.lookupOrDie("FIRST_CUT_SITE"))
-SECOND_CUT_SITE=int(config.lookupOrDie("SECOND_CUT_SITE"))
-DELETION_REGION=Interval(FIRST_CUT_SITE,SECOND_CUT_SITE)
-TARGET_CHROM=config.lookupOrDie("TARGET_CHROM")
-MAX_ANCHOR_DISTANCE=int(config.lookupOrDie("MAX_ANCHOR_DISTANCE"))
-#MAX_ANCHOR_OVERLAP=int(config.lookupOrDie("MAX_ANCHOR_OVERLAP"))
-
-# Open the ENCODE alignability map
-bigwig=pyBigWig.open(alignabilityMap)
+# Instantiate Ahab object
+ahab=Analysis(configFilename)
 
 # Process SAM file
 hspFactory=SamHspFactory()
 stream=SamPairedReadStream(samFile)
-#readsKept=0
-#ALIGNABILITIES=open("alignabilities.txt","wt")
-#REFGAPS=open("ref-gaps.txt","wt")
+readsKept=0
 while(True):
     readGroup=stream.nextGroup()
     if(readGroup is None): break
-    firstReads=getReadEnds(readGroup,1)
+    firstReads=readGroup.getReadEnds(1)
 
     # Should collapse these down to a single line based on a
     # SamAnnotationFactory:
@@ -159,23 +148,15 @@ while(True):
 
     # Filter based on alignment quality and target chromosome
     if(not anno.allRefsSame()): continue
-    if(anno.firstRef()!=TARGET_CHROM): continue
-    if(anno.lowestPercentIdentity()<MIN_IDENTITY): continue
-    getAlignabilities(anno,bigwig)
-    if(anno.getLowestAlignability()<MIN_ALIGNABILITY): continue
+    if(anno.firstRef()!=ahab.TARGET_CHROM): continue
+    if(anno.lowestPercentIdentity()<ahab.MIN_IDENTITY): continue
+    ahab.getAlignabilities(anno)
+    if(anno.getLowestAlignability()<ahab.MIN_ALIGNABILITY): continue
     if(anno.anyRefsOverlap()): continue
 
     # Address cases of 1 HSP, 2 HSPs, 3 HSPs, and >3 HSPs
-    numHSPs=anno.numHSPs()
-    if(numHSPs==1): process1HSP(anno)
-    elif(numHSPs==2): process2HSPs(anno)
-    elif(numHSPs==3): process3HSPs(anno)
-    else: processManyHSPs(anno)
+    ahab.processCases(anno)
 
-    #if(not anno.allSameStrand()): continue
-
-    #readGapLengths=anno.getReadGapLengths()
-    #refGapLengths=anno.getRefGapLengths()
-    #if(len(refGapLengths)>0 and max(refGapLengths)>MAX_REF_GAP): continue
-    
+print(readsKept,"reads kept")    
+del ahab # Call destructor to clean up
 
