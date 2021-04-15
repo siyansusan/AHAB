0f80d6728c55cd3d4d1b2bb52fcfa2872c2398aa
siyansusan <siyan.liu435@duke.edu>
2021-04-15 02:04:33 -0400

    Refactor code

--- Ahab.py
+++ Ahab.py
@@ -1,51 +1,47 @@
-#=========================================================================
+# =========================================================================
 # This is OPEN SOURCE SOFTWARE governed by the Gnu General Public
 # License (GPL) version 3, as described at www.opensource.org.
 # Copyright (C)2016 William H. Majoros (martiandna@gmail.com).
-#=========================================================================
+# Author: Siyan Liu
+# =========================================================================
 from __future__ import (absolute_import, division, print_function,
-   unicode_literals, generators, nested_scopes, with_statement)
-from builtins import (bytes, dict, int, list, object, range, str, ascii,
-   chr, hex, input, next, oct, open, pow, round, super, filter, map, zip)
-import os
+                        unicode_literals, generators, nested_scopes, with_statement)
+
 from Rex import Rex
-rex=Rex()
-import pyBigWig
+rex = Rex()
 from ConfigFile import ConfigFile
 from Strand import Strand
 
-#=========================================================================
-# Class Ahab
-# 
-# This class should be used to encapsulate common functionality across all
-# different analysis scripts.
-#
-# Attributes:
-#   readsBinned : int
-# Instance Methods:
-#   ahab=Ahab(OUTPUT_DIR)
-#   ahab.bin(Annotation,FILE)
-#   ahab.dump(Annotation,FILE)
-#   ahab.getAlignabilities(anno)
-# Class Methods:
-#   none
-# Private methods:
-#=========================================================================
-class Ahab:
-    """Ahab"""
-    def __init__(self,configFile):
-        self.config=ConfigFile(configFile)
-        alignabilityMapFile=self.config.lookupOrDie("ALIGNABILITY")
-        self.bigwig=pyBigWig.open(alignabilityMapFile)
-        #print(self.bigwig.chroms())
-        self.readsBinned=0
-        self.CHROMS=set()
 
-    # This method prints out debugging information for the HSPs of a read
-    def dump(self,anno,FILE):
-        HSPs=anno.getHSPs()
-        numHSPs=len(HSPs)
-        print(anno.getReadID(),numHSPs,sep="\t",file=FILE,flush=True)
+class Ahab:
+    """
+    This class should be used to encapsulate common functionality across all
+    different analysis scripts.
+
+    Attributes:
+        readsBinned : int
+    Instance Methods:
+        ahab=Ahab(OUTPUT_DIR)
+        ahab.bin(Annotation,FILE)
+        ahab.dump(Annotation,FILE)
+        ahab.getAlignabilities(anno)
+    Class Methods:
+        none
+    Private methods:
+    """
+
+    def __init__(self, configFile):
+        self.config = ConfigFile(configFile)
+        self.readsBinned = 0
+        self.CHROMS = set()
+
+    def dump(self, anno, FILE):
+        """
+        This method prints out debugging information for the HSPs of a read.
+        """
+        HSPs = anno.getHSPs()
+        numHSPs = len(HSPs)
+        print(anno.getReadID(), numHSPs, sep="\t", file=FILE, flush=True)
         for hsp in HSPs:
             print("\t",
                   hsp.getRefName(),
@@ -56,37 +52,38 @@ class Ahab:
                   hsp.getPercentIdentity(),
                   hsp.getAlignability(),
                   hsp.getSeq(),
-                  sep="\t",file=FILE,flush=True)
+                  sep="\t", file=FILE, flush=True)
 
-    # This method bins a read by writing into a bin file
-    def bin(self,anno,FILE):
-        readSeq=anno.getSamRecord().getSequence() ### temporary
-        print(anno.getReadID(),readSeq,sep="\t",file=FILE,flush=True) ### temp
+    def bin(self, anno, FILE):
+        """
+        This method bins a read by writing into a bin file.
+        """
+        readSeq = anno.getSamRecord().getSequence()
+        print(anno.getReadID(), readSeq, sep="\t", file=FILE, flush=True)
+        self.readsBinned += 1
 
-        #print(anno.getReadID(),file=FILE,flush=True)
-        self.readsBinned+=1
-
-    def getMinAlignability(self,A):
-        a=[]
+    def getMinAlignability(self, A):
+        """
+        Find the minimum alignability.
+        """
+        a = []
         for x in A:
-            if(x is not None): a.append(x)
-        if(len(a)>0): return min(a)
+            if x is not None:
+                a.append(x)
+        if len(a) > 0:
+            return min(a)
         return 0
 
-    # This calls bigwit.stats() to get the alignabilities for all windows
-    # overlapping any HSPs in this annotation
-    def getAlignabilities(self,anno):
+
+    def getAlignabilities(self, anno):
+        """
+        This calls bigwit.stats() to get the alignabilities for all windows
+        overlapping any HSPs in this annotation
+        """
         for hsp in anno.getHSPs():
-            if(hsp.getRefName() in self.CHROMS):
-                refCoords=hsp.getRefInterval()
-                #print("BIGWIG QUERY:",hsp.getRefName(),refCoords.getBegin(),
-                #      refCoords.getEnd(),sep="\t")
-                stats=self.bigwig.stats(hsp.getRefName(),refCoords.getBegin(),
-                                        refCoords.getEnd(),type="min")
-                #minValue=min(stats)
-                minValue=self.getMinAlignability(stats)
+            if hsp.getRefName() in self.CHROMS:
+                refCoords = hsp.getRefInterval()
+                stats = self.bigwig.stats(hsp.getRefName(), refCoords.getBegin(),
+                                          refCoords.getEnd(), type="mean")
+                minValue = self.getMinAlignability(stats)
                 hsp.setAlignability(minValue)
-
-                
-
-
